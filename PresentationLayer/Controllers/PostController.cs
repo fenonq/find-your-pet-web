39cc6ec8ad0bc43041c399ad389b49994af385dd
Oksana Lysko <lysko.oksanka@gmail.com>
2023-05-09 15:02:11 +0300

    Login and Registration pages

--- PresentationLayer/Controllers/PostController.cs
+++ PresentationLayer/Controllers/PostController.cs
@@ -51,9 +51,7 @@ public class PostController : Controller
     }
 
     [HttpGet]
-    //[AllowAnonymous]
-    [Authorize(Roles = "admin")] // to test roles
-
+    [AllowAnonymous]
     public IActionResult AllPosts(string sortOrder)
     {
         _logger.LogInformation("Show AllPosts..");
